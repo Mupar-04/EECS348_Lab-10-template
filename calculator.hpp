4ef94d8730c380959dd3cb49eba573e71d0227bc
Marco1216 <mupar1216@ku.edu>
2025-04-17 10:29:06 -0500

    Added function declarations for my own use.

--- calculator.hpp
+++ calculator.hpp
@@ -6,5 +6,8 @@
 // this function should expect that `expression` has already been
 // validated and thus throw no exceptions
 double parse_number(const std::string &expression);
+bool isValidDouble(const std::string &s);
+std::string addDoubles(const std::string &a, const std::string &b);
+std::vector<std::string> readNumbersFromFile(const std::string &filename);
 
 #endif // __CALCULATOR_HPP
